7e8c9b75ae8241012f52a4befea9366a32910ab8
sayak <Sayakraha12@gmail.com>
2023-07-10 14:07:09 +0530

    file system revamp

--- FSmodule.js
+++ FSmodule.js
@@ -11,7 +11,7 @@ if (!(fs.existsSync("./myfiles/hello.txt"))) {
     });
 }
 
-//Read (if file does not exist, ReadFile will log it)
+// Read (if file does not exist, ReadFile will log it)
 
 if (fs.existsSync("./myfiles/hello.txt")) {
     fs.readFile("./myfiles/hello.txt", "utf8", (err, data) => {
@@ -39,3 +39,57 @@ if (fs.existsSync("./myfiles/hello.txt")) {
 else{
     console.log("File does not exist");
 }
+
+
+if (!fs.existsSync("./myfiles/hello.txt")) {
+    fs.writeFile("./myfiles/hello.txt", "hello student, how was you day?",() => {
+       
+        console.log("Create file Success");
+    });
+}
+else{
+    console.log("file already exists, Deleting the file...");
+    fs.unlink("./myfiles/hello.txt",(err)=>{
+        if (err) {
+            console.log(err);
+        }
+        console.log("File Deleted");
+    });
+}
+
+// Read (if file does not exist, ReadFile will log it)
+
+if (fs.existsSync("./myfiles/hello.txt")) {
+    fs.readFile("./myfiles/hello.txt", "utf8", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(data);
+        }
+    });
+}
+else{
+    console.log("File does not exist");
+}
+
+
+// make directory or folders
+
+if (!fs.existsSync("./assets")) {
+    fs.mkdir("./assets", (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log("Folder Created");
+})
+}
+else{
+    console.log("Folder already exists, deleting the folder instead...");
+    fs.rmdir("./assets", (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Folder Deleted");
+    })
+}
+
