bac03d05ef2d742c50c5038c5575eacf154bf546
sayak <Sayakraha12@gmail.com>
2023-09-19 11:38:09 +0530

    node

--- FSmodule.js
+++ FSmodule.js
@@ -1,6 +1,16 @@
 // const os = require('os');
 // console.log(os.platform(), os.homedir());
-
+//Filesystem works
+// files:
+// create +
+// read +
+// update +
+// delete +
+// rename
+// folder:
+// create +
+// list
+// delete +
 const fs = require('fs');
 //Create (if file does not exist, WriteFile will create it)
 
@@ -10,68 +20,68 @@ const fs = require('fs');
 //         console.log("Create file Success");
 //     });
 // }
-if (!fs.existsSync("./myfiles/hello.txt")) {
-    fs.writeFile("./myfiles/hello.txt", "hello student, how was you day?",() => {
+// if (!fs.existsSync("./myfiles/hello.txt")) {
+//     fs.writeFile("./myfiles/hello.txt", "hello student, how was you day?",() => {
        
-        console.log("Create file Success");
-    });
-}
-else{
-    console.log("file already exists, Deleting the file...");
-    fs.unlink("./myfiles/hello.txt",(err)=>{
-        if (err) {
-            console.log(err);
-        }
-        console.log("File Deleted");
-    });
-}
-// Read (if file does not exist, ReadFile will log it)
+//         console.log("Create file Success");
+//     });
+// }
+// else{
+//     console.log("file already exists, Deleting the file...");
+//     fs.unlink("./myfiles/hello.txt",(err)=>{
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log("File Deleted");
+//     });
+// }
+// // Read (if file exists, ReadFile will log it)
 
-if (fs.existsSync("./myfiles/hello.txt")) {
-    fs.readFile("./myfiles/hello.txt", "utf8", (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(data);
-        }
-    });
-}
-else{
-    console.log("File does not exist");
-}
+// if (fs.existsSync("./myfiles/hello.txt")) {
+//     fs.readFile("./myfiles/hello.txt", "utf8", (err, data) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(data);
+//         }
+//     });
+// }
+// else{
+//     console.log("File does not exist");
+// }
+
+// update file/append file
+// if (fs.existsSync("./myfiles/hello.txt")) {
+//     fs.appendFile("./myfiles/hello.txt", " My day was awesome!", (err) => {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log("Append Successful");
+//         }
+//     });
+// }
+// else{
+//     console.log("File does not exist");
+// }
 
 
-if (fs.existsSync("./myfiles/hello.txt")) {
-    fs.appendFile("./myfiles/hello.txt", " My day was awesome!", (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log("Append Successful");
-        }
-    });
-}
-else{
-    console.log("File does not exist");
-}
+// // make directory or folders
 
-
-// make directory or folders
-
-if (!fs.existsSync("./assets")) {
-    fs.mkdir("./assets", (err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log("Folder Created");
-})
-}
-else{
-    console.log("Folder already exists, deleting the folder instead...");
-    fs.rmdir("./assets", (err) => {
-        if (err) {
-            console.log(err);
-        }
-        console.log("Folder Deleted");
-    })
-}
+// if (!fs.existsSync("./assets")) {
+//     fs.mkdir("./assets", (err) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log("Folder Created");
+// })
+// }
+// else{
+//     console.log("Folder already exists, deleting the folder instead...");
+//     fs.rmdir("./assets", (err) => {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log("Folder Deleted");
+//     })
+// }
 
