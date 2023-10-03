91e349e2968dec4771429b9f7ed8d55ba008318c
sayak <Sayakraha12@gmail.com>
2023-10-03 13:00:36 +0530

    crud complete

--- expresscrud/controllers/blogfunctions.js
+++ expresscrud/controllers/blogfunctions.js
@@ -14,7 +14,7 @@ const blog_create_post = (req, res)=>{
     const body = req.body;
     var newblog = new Blog({
       name: body.name,
-      review: body.review
+      review: body.review,
     }) ;
   
     
@@ -30,7 +30,38 @@ const single_blog_index =(req, res)=>{
       res.render("details", {ti});
     })
   }
+  const blog_update_get =(req, res)=>{
+    const id = req.params.id;
+    Blog.findById(id).then(result=>{
+      const ti = result;
+      res.render("update", {ti});
+    })
+    
+}
+  const blog_update_post =(req, res)=>{
+    const id = req.params.id;
+    const body = req.body;
+    Blog.findByIdAndUpdate(id, {name: body.name, review: body.review}).then((err, result)=>{
+      if (err) {
+        console.log("Some error occured while updating", err);
+      }
+      console.log("record updated successfully");
+      res.redirect("/");
+    });
+    
+}
+  const blog_delete =(req, res)=>{
+    const id = req.params.id;
+    Blog.findByIdAndDelete(id).then((result)=>{
+      console.log("record deleted successfully");
+      res.redirect("/");
+    });
+    
+}
   module.exports = {blog_index,
 blog_create_get,
 blog_create_post,
-single_blog_index};
+single_blog_index,
+blog_update_get,
+blog_update_post,
+blog_delete};
--- expresscrud/models/Blogroutes.js
+++ expresscrud/models/Blogroutes.js
@@ -9,4 +9,7 @@ router.get("/add-review", functions.blog_create_get)
 router.post("/add-review", functions.blog_create_post)
 
 router.get("/reviews/:id", functions.single_blog_index)
+router.get("/update-reviews/:id", functions.blog_update_get)
+router.post("/update-reviews/:id", functions.blog_update_post)
+router.get("/delete-reviews/:id", functions.blog_delete)
 module.exports = router;
